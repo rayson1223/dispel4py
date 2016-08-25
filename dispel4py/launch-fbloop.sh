1db047c3f8663a53dc6a2784197d582ba9458c5f
aspinuso <spinuso@knmi.nl>
2016-08-25 11:25:37 +0200

    removed test path

--- dispel4py/launch-fbloop.sh
+++ dispel4py/launch-fbloop.sh
@@ -1,4 +1,4 @@
 #python -m dispel4py.new.processor simple undetermined-tst 
-python -m dispel4py.new.processor multi test.feedbackloop.feedbackloop -n 20 -f undetermined-input
+python -m dispel4py.new.processor multi feedbackloop -n 20 -f undetermined-input
 
 #mpiexec-openmpi-mp python -m dispel4py.new.processor multi feedbackloop -n 12 -f undetermined-input 
